cf10f57712bf5780ae3108dcff5131c233c0e86b
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-19 00:22:54 +0200

    Added bootstrap and basic tempaltes

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Question, Level, Answer
 
 # Create your views here.
+
+
+def start_game(request):
+    questions = Question.objects.all()
+
+    return render(request, "start_game.html", {"questions": questions})
