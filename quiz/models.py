de0ef5d4adbfb5c489a9100e4c415077232981f2
Danilo <danilo.koljensic.fit@gmail.com>
2025-04-18 23:50:22 +0200

    Added Question, Answer, Level model for quiz game

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Level(models.Model):
+    type = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.type
+
+
+class Question(models.Model):
+    level = models.ForeignKey(Level, related_name="questions", on_delete=models.CASCADE)
+    text = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=150)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(
+        Question, related_name="answers", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.text
